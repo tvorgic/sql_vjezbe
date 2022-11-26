3cfe53df7e1071eefebc118f9eb5cb93c7f7e421
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2022-11-26 15:41:04 +0100

    Update knjiznica.sql
    nastavak vježbe po skripti. unos indexa i kreiranje  er dijagrama

--- knjiznica.sql
+++ knjiznica.sql
@@ -35,3 +35,48 @@ create table katalog(
     izdavac int,
     mjesto int
 );
+
+#slijedi alter table niz naredbi radi novog unosa tablica sa vrijednostima.
+#slijedi drop table niz naredbi radi brisanja tablica i novog unosa tablica sa vrijednostima. inače se unos radi za vrijeme kreiranja tablice
+
+alter table autor change sifra sifra int not null;
+
+drop table autor;
+drop table izdavac;
+drop table mjesto;
+drop table katalog;
+
+create table autor(
+    sifra int not null primary key,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    datumrodenja datetime
+);
+
+create table izdavac(
+    sifra int not null primary key,
+    naziv varchar(50) not null,
+    aktivan boolean
+);
+
+create table mjesto(
+    sifra int not null primary key,
+    naziv varchar(50) not null,
+    postanskiBr varchar(10) not null,
+    drzava varchar(50) not null
+);
+
+create table katalog(
+    sifra int not null primary key,
+    autor int,
+    naslov varchar(100) not null,
+    izdavac int,
+    mjesto int,
+    cijena decimal(18,2)
+);
+
+alter table katalog add foreign key (autor) references autor(sifra);
+alter table katalog add foreign key (izdavac) references izdavac(sifra);
+alter table katalog add foreign key (mjesto) references mjesto(sifra);
+
+create index naziv_index on katalog(naslov);
