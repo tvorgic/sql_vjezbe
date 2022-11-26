19f3f94595c2a45129319267662f030452b7e8d7
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2022-11-26 09:37:39 +0100

    Update knjiznica.sql
    kreiranje tablica. slijede izmjene

--- knjiznica.sql
+++ knjiznica.sql
@@ -14,3 +14,24 @@ create table autor(
     prezime varchar(50),
     datumrodenja datetime
 );
+
+create table izdavac(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    aktivan boolean
+);
+
+create table mjesto(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    postanskiBr varchar(10),
+    drzava varchar(50)
+);
+
+create table katalog(
+    sifra int not null primary key auto_increment,
+    autor int,
+    naslov varchar(50),
+    izdavac int,
+    mjesto int
+);
